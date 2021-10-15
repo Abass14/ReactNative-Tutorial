817de32246ef417b5fe7c4300958a3380770ef53
Abass14 <abasstoy14@gmail.com>
2021-10-15 02:23:23 +0100

    completed list, scrollview and refresh control lesson

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import React, { useState } from 'react';
 import {
   Button,
   Linking,
+  RefreshControl,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -28,125 +29,75 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-
-
 const App = () => {
+  const [items, setItems] = useState([
+    {key: 1, item: 'Item 1'},
+    {key: 2, item: 'Item 2'},
+    {key: 3, item: 'Item 3'},
+    {key: 4, item: 'Item 4'},
+    {key: 5, item: 'Item 5'},
+    {key: 6, item: 'Item 6'},
+    {key: 7, item: 'Item 7'},
+    {key: 8, item: 'Item 8'},
+    {key: 9, item: 'Item 9'},
+    {key: 10, item: 'Item 10'},
+    {key: 11, item: 'Item 11'},
+    {key: 12, item: 'Item 12'},
+    {key: 13, item: 'Item 13'},
+    {key: 14, item: 'Item 14'},
+  ]);
+
+  const [Resfreshing, setRefresh] = useState(false)
+  const onRefresh =()=>{
+    setRefresh(true);
+    setItems([...items, {key: 15, item: 'Item 100'}]);
+    setRefresh(false);
+  }
 
   return (
     <View style={styles.body}>
-      <View style={styles.subBody}>
-        <View style={styles.subBodyRow}>
-          <View style={styles.view1}>
-            <Text style={styles.text}>1</Text>
-          </View>
-          <View style={styles.view2}>
-            <Text style={styles.text}>2</Text>
-          </View>
-          <View style={styles.view3}>
-            <Text style={styles.text}>3</Text>
-          </View>
-        </View>
-        
-        <View style={styles.subBody2}>
-          <Text style={styles.text}>4</Text>
-        </View>
-        <View style={styles.subBody3}>
-          <Text style={styles.text}>5</Text>
-        </View>
-      </View>
-
-      
-
-      <View style={styles.subBody4}>
-        <View style={styles.view4}>
-          <Text style={styles.text}>6</Text>
-        </View>
-        <View style={styles.view5}>
-          <Text style={styles.text}>7</Text>
-        </View>
-      </View>
-    
-    
-      
+      <ScrollView 
+        refreshControl={
+          <RefreshControl 
+            refreshing={ Resfreshing }
+            onRefresh= {onRefresh}
+            colors={['red']}
+          />
+        }
+      >
+        {
+          items.map((i)=>{
+              return (
+                <View style={styles.item} key={i.key}>
+                <Text style={styles.text}>{i.item}</Text>
+              </View>
+              )
+          })
+        }
+      </ScrollView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   body: {
-    flex: 1,
     flexDirection: 'column', 
   },
 
-  subBody: {
-    flex: 2,
-    flexDirection: 'column',
-  },
-
-  subBodyRow:{
-    flex: 1,
-    flexDirection: 'row'
-  },
-
-  subBody2: {
-    flex: 1,
-    flexDirection: 'row',
+  item:{
     backgroundColor: 'yellow',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-
-  subBody3: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  subBody4: {
-    flex: 3,
-    flexDirection: 'row',
-    alignItems: 'stretch',
-    justifyContent: 'center',
-  },
-
-  view4: {
-    flex: 1,
-    backgroundColor: 'yellow',
-    alignItems: 'center',
-    justifyContent: 'center'
-  
-  },
-  view5: {
-    flex: 1,
-    backgroundColor: 'blue',
-    alignItems: 'center',
-    justifyContent: 'center'
+    margin: 10
   },
 
   text: {
     color: 'black',
-    fontSize: 20
-  },
-  view1: {
-    flex: 1,
-    backgroundColor: 'green',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  view2: {
-    flex: 2,
-    backgroundColor: 'blue',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  view3: {
-    flex: 3,
-    backgroundColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
+    fontSize: 35,
+    fontStyle: 'italic',
+    margin: 10
+  }
+
 
 
 
