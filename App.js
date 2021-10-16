35f889b3b5853a4e69b74d831396cfb46c24ac66
Abass14 <abasstoy14@gmail.com>
2021-10-16 01:11:57 +0100

    completed excercise

--- App.js
+++ App.js
@@ -49,33 +49,19 @@ const App = () => {
     {name: 'Item 14'},
   ]);
 
-  const DATA = [
+
+  const [DATA, setData] = useState([
     {
-      title: 'Title 1',
-      data: ['Item 1-1', 'Item 1-2', 'Item 1-3' ]
-    },
-    {
-      title: 'Title 2',
-      data: ['Item 2-1', 'Item 1-2', 'Item 1-3' ]
-    },
-    {
-      title: 'Title 3',
-      data: ['Item 3-1', 'Item 3-2' ]
-    },
-    {
-      title: 'Title 4',
-      data: ['Item 4-1']
-    },
-    {
-      title: 'Title 5',
-      data: ['Item 5-1', 'Item 5-2', 'Item 5-3' ]
-    },
-  ];
+      title: `Title ${1}`,
+      data: [`Item 1-1`, `Item 1-2`, `Item 1-3` ]
+    }
+  ]);
 
   const [Resfreshing, setRefresh] = useState(false)
+
   const onRefresh =()=>{
     setRefresh(true);
-    setItems([...items, {name: 'Item 100'}]);
+    setData([...DATA, {title: `Title ${DATA.length + 1}`, data: [`Item ${DATA.length + 1}-1`, `Item ${DATA.length + 1}-2`]}]);
     setRefresh(false);
   }
 
@@ -84,6 +70,12 @@ const App = () => {
 
       <SectionList
         sections= {DATA}
+        refreshControl={
+          <RefreshControl
+            refreshing= {Resfreshing}
+            onRefresh= {onRefresh}
+          />
+        }
         keyExtractor={(item, index)=>index.toString()}
         renderItem={({ item })=>(
           <View style={styles.itemChild}>
